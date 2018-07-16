4456b4efa5d238e5442aa6a2d64111c8d626d2ea
xlsn0w <xlsn0wios@gmail.com>
2018-07-16 15:22:36 +0800

    init

---
+++ Encrypt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Encrypt
+//
+//  Created by HL on 2018/7/16.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
